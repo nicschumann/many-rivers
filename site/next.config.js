c06ec5a8fd740e3a712d6690d1816b7dd69ce7b4
nicschumann <nic@nicschumann.co>
2023-10-09 14:36:06 +0900

    [feat] redirects home to rivers with next.config

--- site/next.config.js
+++ site/next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, options) => {
-        config.module.rules.push({
-            test: /\.(frag|vert)$/i,
-            use: 'raw-loader'
-        })
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(frag|vert)$/i,
+      use: "raw-loader",
+    });
 
-        return config
-    }
-}
+    return config;
+  },
+  async redirects() {
+    return [{ source: "/", destination: "/rivers/el-horcon", permanent: true }];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
