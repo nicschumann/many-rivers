bc8eb8006386af3f2cb64499f5cd773262b9f941
nicschumann <nic@nicschumann.co>
2023-11-22 12:14:15 +0900

    [feat] adds modal functionality to sim tool view

--- site/components/SimulationToolsOverlay/SimulationToolsOverlay.tsx
+++ site/components/SimulationToolsOverlay/SimulationToolsOverlay.tsx
@@ -8,6 +8,7 @@ import { River } from "@/simulation/data/rivers";
 import SimulationToolsDisplayModes from "../SimulationToolsDisplayModes/SimulationToolsDisplayModes";
 import PointerLockButton from "../PointerLockButton/PointerLockButton";
 import FooterRow from "../FooterRow/FooterRow";
+import ModalOverlay from "../ModalOverlay/ModalOverlay";
 
 interface SimulationToolsOverlayProps {
   t: number;
@@ -23,8 +24,8 @@ export default function SimulationToolsOverlay({
   const [nextRiver, setNextRiver] = useState<River>(river);
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
+  const [wasRunning, setWasRunning] = useState(false);
   const isRunning = useApplicationState((s) => s.sim.state.running);
-
   const setRunning = useApplicationState((s) => {
     return (running: boolean) => {
       s.setSimState({ running });
@@ -36,6 +37,17 @@ export default function SimulationToolsOverlay({
       s.setUIState({ active_overlay: newOverlayState });
     };
   });
+
+  const openModal = () => {
+    setWasRunning(isRunning);
+    setRunning(false);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    setRunning(wasRunning);
+  };
 
   const renderState = useApplicationState((s) => {
     return {
@@ -106,8 +118,47 @@ export default function SimulationToolsOverlay({
         w={w}
         isRunning={isRunning}
         setRunning={setRunning}
-        setModalIsOpen={setModalIsOpen}
+        openModal={openModal}
       />
+      {modalIsOpen && (
+        <ModalOverlay closeModal={closeModal}>
+          <div className="mt-8 indent-10">Project Description</div>
+          <div className="mt-8 indent-10">
+            The project consists of a computational model that aims to
+            anticipate the future geomorphology of the Rio Grande / Río Bravo
+            and, with it, of the U.S.-Mexico border. Taking into account the
+            physics that participate in the large-scale evolution of the river,
+            the program generates speculative landscapes by simulating complex
+            interactions between water and land in specific places and across
+            geological temporalities.
+          </div>
+          <div className="indent-10">
+            A pipeline of several computational steps calculates the capacities
+            of water to erode land and of land to accumulate sediments. As the
+            river&rsquo;s waters push towards the inside of its meanders, the
+            outer bends smooth out, generating an intensifying feedback loop
+            that results in geometric transformations. The shape of the
+            simulated rivers evolve over time using a dense grid of
+            computational cells with locally-defined rules. The volume of water
+            in each cell changes in response to the gradients of land and water
+            surrounding it — carrying materials into or away from it. The flows
+            of these materials affect such gradients where water determines
+            where sediments concentrate while land affects water behaviors.
+          </div>
+          <div className="indent-10">
+            Using topographic scans from the US Geological Survey, the simulator
+            explores the ever-changing course of the river as a border
+            technology. The border expands and contracts permanently. It is
+            dynamically renegotiated where each act of measurement allows for
+            the recalibration of the authority exercised over it. Given its
+            instability, it would seem as if rivers wouldn&rsquo;t be the best
+            way to demarcate borders. However such movement, paradoxically,
+            generates the conditions that are used to justify the application of
+            force over it — the operational regimes that reinforce the river as
+            an artificial limit.
+          </div>
+        </ModalOverlay>
+      )}
     </div>
   );
 }
