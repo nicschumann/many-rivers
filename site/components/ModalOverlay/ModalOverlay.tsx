7797f595e579d92e95b1db9f1e6102c3466a065a
nicschumann <nic@nicschumann.co>
2023-11-24 15:13:02 +0900

    [feat] reconciles modal close button with the graphic style

--- site/components/ModalOverlay/ModalOverlay.tsx
+++ site/components/ModalOverlay/ModalOverlay.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import OverlayButton from "../OverlayButton/OverlayButton";
 
 interface ModalOverlayProps {
   children?: React.ReactNode;
@@ -23,23 +24,12 @@ export default function ModalOverlay({
 
   return (
     <div className="absolute z-10 top-0 left-0 w-screen h-screen my-auto overflow-scroll">
-      <div className="border-white border text-white m-8 pt-8 pb-16 px-20 rounded-md overflow-scroll">
+      <div className="border-white border text-white m-8 xl:mx-72 lg:mx-32 pt-8 pb-16 px-20 rounded-md overflow-scroll">
         <div className="text-right">
           <div onClick={closeModal}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6 ml-auto text-white hover:text-black cursor-pointer hover:bg-white rounded-xl"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
+            <OverlayButton>
+              <span className="px-[0.2rem]">X</span>
+            </OverlayButton>
           </div>
         </div>
         <div className="h-full overflow-hidden">{children}</div>
