ca121bbeeaa223e399d4eef2e5a50047619f8553
nicschumann <nic@nicschumann.co>
2023-11-26 16:17:56 +0900

    [trivial] adds black background on html element

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-black w-screen h-screen">
       <body>
         {children}
         <Analytics />
