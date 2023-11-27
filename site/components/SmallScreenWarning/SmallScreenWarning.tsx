e260c6c32f0e764b483617f914229d01cd9cc4f7
nicschumann <nic@nicschumann.co>
2023-11-27 11:15:54 +0900

    [feat] adds generic small screen warning

---
+++ site/components/SmallScreenWarning/SmallScreenWarning.tsx
@@ -0,0 +1,10 @@
+export default function SmallScreenWarning() {
+  return (
+    <div className="block sm:hidden bg-black p-4 text-center w-screen h-screen fixed top-0 left-0 z-20">
+      <p className="text-white relative top-[50%]">
+        This simulation is resource-intensive, and is not intended for small
+        screens. Please visit the site on desktop with a larger window size.
+      </p>
+    </div>
+  );
+}
