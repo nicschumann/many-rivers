5bf3f8b5bb4512cb9bd7f7aae45b9e5f01931c19
nicschumann <nic@nicschumann.co>
2023-11-28 10:24:45 +0900

    [feat] updates project description, adds google analytics.

--- site/components/SmallScreenWarning/SmallScreenWarning.tsx
+++ site/components/SmallScreenWarning/SmallScreenWarning.tsx
@@ -1,8 +1,8 @@
 export default function SmallScreenWarning() {
   return (
     <div className="block sm:hidden bg-black p-4 text-center w-screen h-screen fixed top-0 left-0 z-20">
-      <p className="text-white relative top-[50%]">
-        This simulation is resource-intensive, and is not intended for small
+      <p className="text-white relative top-[25%]">
+        This simulation is resource-intensive and is not intended for small
         screens. Please visit the site on desktop with a larger window size.
       </p>
     </div>
